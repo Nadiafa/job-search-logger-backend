053415ef8633d1e9543d11cd1b22d9ef7528a336
Nadiafa <nadiafa581@gmail.com>
2019-05-28 18:20:39 +0100

    Add CRUD actions to roles_controller.rb

--- app/controllers/api/roles_controller.rb
+++ app/controllers/api/roles_controller.rb
@@ -1,2 +1,46 @@
 class Api::RolesController < ApplicationController
+	before_action :set_role, only: [:show, :update, :destroy]
+	
+	def index 
+		render json: Role.all
+	end
+
+	def show
+		render json: @role
+	end
+
+	def create
+		role = Role.new(role_params)
+		if role.save
+			render json: role
+		else
+			render json: { message: role.errors }, status: 400
+		end
+	end
+
+	def update
+		if @role.update(role_params)
+			render json: @role
+		else
+			render json: { message: @role.errors }, status: 400
+		end
+	end
+
+	def destroy
+		if @role.destroy 
+			render json: { message: "Successfully Destroyed" }, status: 204
+		else
+			render json: { message: "Unable to Destroy" }, status: 400
+		end
+	end
+
+	private
+
+	def role_params
+		params.require(:role).permit(:title, :description, :requirements, :location, :company, :closing_date)
+	end
+
+	def set_role
+		@role = Role.find_by(id: params[:id])
+	end
 end
