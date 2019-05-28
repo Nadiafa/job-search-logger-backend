731bdf27ab14147c096cdd4e0191fb1f23c33474
Nadiafa <nadiafa581@gmail.com>
2019-05-28 18:34:52 +0100

    Change rails server port to 3001 in config/initializers/puma.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Role.create([
+	{
+	title: 'Web Developer',
+	description: 'We have a great position for a web developer in a fast growing Company based in London Soho for an enthusiastic, self-motivated and willing to express their individual skills. As a Web Developer you will be working with the design, development, and maintenance of Websites. An opportunity that will allow you to gain valuable experience in programing. The successful candidate will work with the main developer to improve the existing projects and launch new ones.',
+	requirements: ['Design, develop and maintain MySQL', 'PHP OOP', 'Code Igniter or similar PHP framework', 'LAMP (Linux, Apache, mySQL, PHP)', 'Front end developing, HTML, CSS and JavaScript.', 'jQuery', 'IT Support/maintenance', 'CMS', 'Photoshop'],
+	company: 'splash group',
+	location: 'London W1D',
+	url:'https://www.indeed.co.uk/cmp/splash-group/jobs/Web-Developer-c1bbe070f1ebd0d1?sjdu=QwrRXKrqZ3CNX5W-O9jEva1Pxk8RxLzF6tbkiLMwjqTeGqZT9ehqbm_LHdbUFx3ncQSedIWvnwU44D1RUgSE0Q&tk=1dbq2ke6b950v802&adid=146979572&vjs=3',
+	status: 'interested'
+	},
+	{
+	title: 'Full stack web developer',
+	description: 'We are currently looking for a full stack web developer with strong front end and back end skills. The successful candidate will be joining our development team and will help in building our various services. The ideal candidate should have strong skills in the latest front-end technologies such as React and CSS pre compilers such as SASS. In addition, they need to be able to work with API’s. In this role a good knowledge of React and PHP are essential and the ability to use Adobe creative suite is a bonus.',
+	requirements: ['AWS and Docker is a MUST!', 'Databases such as MangoDB and MySQL', 'Experience within the Financial industry is plus, like payment system', 'Adobe Creative Suite (photoshop, XD, Illustrator)', 'HTML5, and CSS3', 'jQuery', 'avaScript, bootstrap & JQuery', 'JavaScript Frameworks such as VUE and Reach', 'JavaScript ES6', 'version control', 'CSS pre-processors such as SASS&LESS'],
+	company: 'Awax Group Ltd',
+	location: 'London',
+	url:'https://www.indeed.co.uk/cmp/AWAX-LTD/jobs/Full-Stack-Web-Developer-6399eb7ed600706f?sjdu=QwrRXKrqZ3CNX5W-O9jEvWHtrk2Yve_I7eN5zDdZnGTCvbYhK58Ns9sMzXVmkoOCk39wYnLQWJ3_uvbuwZwchM09qAgXKvuiHb3ehQGh0wo&tk=1dbq2ke6b950v802&adid=292468265&vjs=3',
+	status: 'interested'
+	},
+	{
+	title: 'Junior Front End Web Developer',
+	description: 'We are a young exciting agency based in the heart of the London who are looking for a passionate Front End Web developer to join our growing team. If you like fast-paced environments then this is the place to be! We work with clients in all different industries and of all shapes and sizes building awesome websites and activating digital marketing programs.',
+	requirements: ['Excellent command of HTML5 & CSS3 with a keen eye for UX design and knowledge of cross-browser compatibility', 'Bootstrap CSS framework', 'JavaScript (vanilla and jQuery)', 'WordPress and experience developing bespoke functionality for websites using PHP', 'Custom Post Types and Custom Fields'],
+	company: 'SoBold Ltd',
+	location: 'London EC4R',
+	url:'https://www.indeed.co.uk/cmp/SoBold-Limited/jobs/Junior-Front-End-Web-Developer-b5a9cdfd5ec9e03a?sjdu=QwrRXKrqZ3CNX5W-O9jEve1dIVOUt-RWe40cYYW649Aa8ZRho4aBMfXf7WVRH2N2tyiHGqA16KjG-az-vpvN-COrZ3D4N_qVN2H7YUAqKSRTrptKa9YCYm54c1x_L8qS&tk=1dbq2ke6b950v802&adid=258903302&vjs=3',
+	status: 'interested'
+	}
+])
+
